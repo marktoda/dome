4b92eae8432951eadd531b0ff242f47b8fb9a2a2
Mark Toda <toda.mark@gmail.com>
2025-04-18 11:54:51 -0400

    feat: cleanup

--- packages/logging/src/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './base';
-export * from './helper';
-export * from './middleware';
-export * from './runWithLogger';
-export type { InitOptions } from './types';
--- packages/logging/src/index.js
+++
@@ -1,5 +0,0 @@
-export * from './base';
-export * from './helper';
-export * from './middleware';
-export * from './runWithLogger';
-//# sourceMappingURL=index.js.map
